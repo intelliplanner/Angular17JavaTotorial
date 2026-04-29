ff7ab505bf0a7ae4ba324a7d8d84bb7f11f0cd4d
Virendra Gupta <virendra.gupta@uneecops.com>
2026-04-29 08:52:19 +0530

    update uri

--- src/app/java-notes/java-notes.component.ts
+++ src/app/java-notes/java-notes.component.ts
@@ -76,9 +76,9 @@ export class JavaNotesComponent implements OnInit {
     
     // In production, use full URL from apiUrl; in development, use relative path
     if (environment.production ) {
-      return `${this.basePath}/Angular17JavaTutorial/assets/javaNotesPdf/${this.selected}`;
+      return `${this.basePath}/assets/javaNotesPdf/${this.selected}`;
     } else {
-      return `${this.basePath}/Angular17JavaTutorial/assets/javaNotesPdf/${this.selected}`;
+      return `${this.basePath}/assets/javaNotesPdf/${this.selected}`;
     }
   }
 
