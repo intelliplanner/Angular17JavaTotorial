a868883767b48446d2b1759cec67aa6449754fe2
Virendra Gupta <virendra.gupta@uneecops.com>
2026-05-07 09:47:36 +0530

    update url

--- src/app/question-answer-panel/question-answer-panel.component.ts
+++ src/app/question-answer-panel/question-answer-panel.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-question-answer-panel',
