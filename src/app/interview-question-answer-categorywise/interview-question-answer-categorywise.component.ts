a0d49399f867229858ea9c2d159761aa4f19d8d6
Virendra Gupta <virendra.gupta@uneecops.com>
2026-05-07 09:44:30 +0530

    update url

--- src/app/interview-question-answer-categorywise/interview-question-answer-categorywise.component.ts
+++ src/app/interview-question-answer-categorywise/interview-question-answer-categorywise.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-interview-question-answer-categorywise',
